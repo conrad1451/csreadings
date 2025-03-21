67d5b7aaa3d48c4fc7eda5c5e1b0b36eb4429a9c
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-03-21 16:39:29 -0400

    feat: added table

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
+import BasicTable from './MyTable'
 import './App.css'
 
-function App() {
+function OldApp() {
   const [count, setCount] = useState(0)
 
   return (
@@ -28,6 +30,15 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+    </>
+  )
+}
+
+function App() {
+  return(
+    <>
+    <BasicTable/>
+    <OldApp/>
     </>
   )
 }
