36b0f7ea7bfd19823474e4a0e095d026cabb6401
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-04-26 14:25:44 -0400

    fix: remove unused import

--- src/MyTable2.tsx
+++ src/MyTable2.tsx
@@ -5,7 +5,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button'; 
+// import Button from '@mui/material/Button'; 
 import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
