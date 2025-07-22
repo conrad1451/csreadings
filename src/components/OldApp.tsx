fec5d72a01d18a086b6eccd7bac1ff7da6305b36
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-07-22 01:12:46 -0400

    fix: correct path for import statements

--- src/components/OldApp.tsx
+++ src/components/OldApp.tsx
@@ -1,10 +1,10 @@
 // src/OldApp.tsx
 
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import reactLogo from "../assets/react.svg";
+import viteLogo from "../../public/vite.svg";
 
-import "./App.css";
+import "../App.css";
 
 export default function OldApp() {
   // <--- ADDED 'export' here
